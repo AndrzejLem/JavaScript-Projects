625934d36bf8c91bf173fba4be11e613e8ad26be
AndrzejLem <andrzejlem1993@gmail.com>
2021-11-23 10:52:37 +0100

    1.

---
+++ 3. Calculator/js/script.js
@@ -0,0 +1,3 @@
+var numberButtons = document.querySelector("data-number");
+var operationButtons = document.querySelector("data-operation");
+var numberButtons = document.querySelector("data-number");
